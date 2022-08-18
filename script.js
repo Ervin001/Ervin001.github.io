fb3997ba78d1aedd4a26f7e6349093b976ebf0de
Ervin <evgnz24@gmail.com>
2022-08-17 23:53:27 -0500

    Started over to do things the right way (The way TOP intended, keeping code in their own scope, and using a ui and controller func)

--- script.js
+++ script.js
@@ -1,215 +1,44 @@
 'use strict';
 
-const gameEl = document.querySelector('.game');
-const playersEl = document.querySelectorAll('.player');
-const playerOneNameEl = document.querySelector('.player-one-name');
-const playerTwoNameEl = document.querySelector('.player-two-name');
-const gameBlurEl = document.querySelector('.game-blur');
-const gameMessageEl = document.querySelector('.game-message');
-const playerWonEl = document.querySelector('.player-won');
-const btnResetEl = document.querySelector('.reset');
-const btnStartOverEl = document.querySelector('.start-over');
-const cellsEL = document.querySelectorAll('[data-cell]');
-const playerOneScoreEl = document.querySelector('.player-one-score');
-const playerTwoScoreEl = document.querySelector('.player-two-score');
-const scoresPlayersEl = document.querySelectorAll('.score');
+const GameBoard = (function (playerOne, playerTwo) {
+  let gameBoard = ['', '', '', '', '', '', '', '', ''];
 
-function Player(name, marker) {
-  const nameUpper = name.toUpperCase();
-  const markerUpper = marker.toUpperCase();
-  let score = 0;
+  const clearBoard = () => {
+    gameBoard = ['', '', '', '', '', '', '', '', ''];
+  };
 
-  const playerScoreFunc = () => score;
+  return { gameBoard, clearBoard };
+})();
 
-  const addTen = () => (score += 10);
+const Game = (() => {
+  let playing = false;
+  let currentPlayer = undefined;
+  let playerOne = undefined;
+  let playerTwo = undefined;
 
-  const removeTen = () => (score -= 10);
+  const start = (p1, p2) => {
+    playerOne = p1;
+    playerTwo = p2;
+    currentPlayer = playerOne;
 
-  return { nameUpper, markerUpper, playerScoreFunc, addTen, removeTen };
+    console.log(GameBoard.gameBoard);
+  };
+
+  const switchPlayer = () => {
+    currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+  };
+
+  return { start };
+})();
+
+const Player = function (name, marker, type) {
+  return { name, marker, type };
+};
+
+function vsPlayer() {
+  const playerOne = Player('Rick', 'X', 'player');
+  const playerTwo = Player('John', 'O', 'player');
+  Game.start(playerOne, playerTwo);
 }
 
-const tom = Player('rick', 'x');
-const comp = Player('computer', 'o');
-
-const game = ((playerOne, playerTwo, gameType) => {
-  let winningConditions = [
-    [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 8],
-    [3, 4, 5],
-    [6, 7, 8],
-    [1, 4, 7],
-    [2, 5, 8],
-    [2, 4, 6],
-  ];
-
-  let gameBoard = ['-', '-', '-', '-', '-', '-', '-', '-', '-'];
-  let playing = true;
-  let players = [playerOne, playerTwo];
-  let activePlayer = 0;
-
-  const getActivePlayer = () => activePlayer;
-
-  const switchActivePlayer = () =>
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-
-  const checkWinner = () => {
-    let roundWon = true;
-    for (let i = 0; i <= 7; i++) {
-      const winCondition = winningConditions[i];
-      let a = gameBoard[winCondition[0]];
-      let b = gameBoard[winCondition[1]];
-      let c = gameBoard[winCondition[2]];
-
-      if (a === '-' || b === '-' || c === '-') {
-        continue;
-      }
-
-      if (a === b && b === c) {
-        gameBlurEl.classList.remove('hide');
-        gameMessageEl.classList.remove('hide');
-        playerWonEl.textContent = `${
-          players[game.getActivePlayer()].nameUpper
-        } Wins!!`;
-        return true;
-      }
-    }
-  };
-  /**
-   * @function gets empty spaces then sets the second players marker in a random empty space
-   */
-  const getEmptySpaces = () => {
-    let newIndexes = [];
-    const markedBoard = gameBoard.forEach((cell, i) => {
-      if (cell === '-') newIndexes.push(i);
-    });
-
-    const indexedNum = Math.trunc(Math.random() * newIndexes.length);
-
-    const newNum = newIndexes[indexedNum];
-
-    if (newNum !== undefined) {
-      cellsEL[newNum].textContent = players[getActivePlayer()].markerUpper;
-      gameBoard[newNum] = '';
-      gameBoard[newNum] = players[1].markerUpper;
-    }
-  };
-
-  const addRandomMarker = () => {
-    // console.log(getEmptySpaces().length);
-  };
-
-  return {
-    playerOne,
-    playerTwo,
-    gameType,
-    players,
-    playing,
-    getActivePlayer,
-    switchActivePlayer,
-    checkWinner,
-    getEmptySpaces,
-    addRandomMarker,
-    gameBoard,
-  };
-})(tom, comp, 'easy');
-
-playerOneNameEl.textContent = game.playerOne.nameUpper;
-playerTwoNameEl.textContent = game.playerTwo.nameUpper;
-
-// Check draw
-// let roundDraw = !gameBoard.includes(undefined)
-
-playersEl[game.getActivePlayer()].style.color = 'red';
-// cellsEL.forEach((cell) =>
-//   cell.addEventListener('click', (e) => {
-//     if (game.playing) {
-//       if (game.gameType === 'easy') {
-//         if (e.target.textContent === '') {
-//           if (game.getActivePlayer() === 0) {
-//             // Show marker on UI
-//             e.target.textContent =
-//               game.players[game.getActivePlayer()].markerUpper;
-//             // Remove previous player color
-//             playersEl[game.getActivePlayer()].style.color = '';
-//             // Add markers to GameBoard State
-//             game.gameBoard[e.target.dataset.cell] =
-//               game.players[game.getActivePlayer()].markerUpper;
-
-//             if (game.checkWinner()) {
-//               game.playing = false;
-//             }
-//             // switch player
-//             game.switchActivePlayer();
-//             playersEl[game.getActivePlayer()].style.color = '';
-//           } else {
-//             playersEl[game.getActivePlayer()].style.color = 'red';
-//             const playerTimer = setTimeout(() => {
-//               // Remove previous player color
-//               game.getEmptySpaces();
-//               game.checkWinner();
-//               game.switchActivePlayer();
-//               playersEl[game.getActivePlayer()].style.color = '';
-//             }, 500);
-//           }
-//           // Change active player color
-//           playersEl[game.getActivePlayer()].style.color = 'red';
-//         }
-//       }
-//     }
-//   })
-// );
-
-// if (game.playing) {
-//   if (game.gameType === 'easy') {
-//     if (game.getActivePlayer() === 0) {
-//       cellsEL.forEach((cell) => {
-//         cell.addEventListener('click', (e) => {
-//           if (e.target.textContent === '') {
-//             // Show marker on UI
-//             e.target.textContent =
-//               game.players[game.getActivePlayer()].markerUpper;
-//             // Remove previous player color
-//             playersEl[game.getActivePlayer()].style.color = '';
-//             // Add markers to GameBoard State
-//             game.gameBoard[e.target.dataset.cell] =
-//               game.players[game.getActivePlayer()].markerUpper;
-
-//             if (game.checkWinner()) {
-//               game.playing = false;
-//             }
-//             playersEl[game.getActivePlayer()].style.color = '';
-//             // switch player
-//             game.switchActivePlayer();
-//           }
-//         });
-//       });
-//     }
-//   }
-// }
-
-if (game.playing) {
-  if (game.gameType === 'easy') {
-    for (let i = 0; i < cellsEL.length; i++) {
-      if (game.getActivePlayer() === 0) {
-        cellsEL[i].addEventListener('click', (e) => {
-          if (e.target.textContent === '') {
-            // Show marker on UI
-            e.target.textContent =
-              game.players[game.getActivePlayer()].markerUpper;
-            // Remove previous player color
-            playersEl[game.getActivePlayer()].style.color = '';
-            // Add markers to GameBoard State
-            game.gameBoard[e.target.dataset.cell] =
-              game.players[game.getActivePlayer()].markerUpper;
-
-            if (game.checkWinner()) {
-              game.playing = false;
-            }
-            game.switchActivePlayer();
-          }
-        });
-      }
-    }
-  }
-}
+vsPlayer();
